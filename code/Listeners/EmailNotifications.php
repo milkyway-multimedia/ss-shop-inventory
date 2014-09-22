f3441d5f5103c038b6d756a11c601351e8548468
Mell <mell@milkywaymultimedia.com.au>
2014-09-22 18:26:53 +1000

    Uploading changes at Mon, 22 Sep 2014 08:26:33 GMT

--- code/Listeners/EmailNotifications.php
+++ code/Listeners/EmailNotifications.php
@@ -12,7 +12,7 @@ namespace Milkyway\SS\Shop\Inventory\Listeners;
 use Milkyway\SS\Shop\Inventory\Extensions\Config;
 
 class EmailNotifications {
-    public function zero($buyable, $orderItem = null) {
+    public function zero($e, $buyable, $orderItem = null) {
         $this->sendNotification([
                 'Subject' => _t('ShopInventory.EmailSubject-NONE', 'NO STOCK FOR: {title}', ['title' => $buyable->Title]),
                 'Body' => _t('ShopInventory.EmailBody-NONE', 'There is no longer any stock for {title}. <a href="{url}">You can update the stock for {title} in the {application}</a>', [
@@ -23,7 +23,7 @@ class EmailNotifications {
                 ], $buyable, $orderItem);
     }
 
-    public function belowIndicator($buyable, $orderItem = null) {
+    public function belowIndicator($e, $buyable, $orderItem = null) {
         $this->sendNotification([
                 'Subject' => _t('ShopInventory.EmailSubject-NONE', 'LOW STOCK FOR: {title}', ['title' => $buyable->Title]),
                 'Body' => _t('ShopInventory.EmailBody-NONE', 'There is only {stock} in stock for {title}, and has fallen below {indicator}. <a href="{url}">You can update the stock for {title} in the {application}</a>', [
